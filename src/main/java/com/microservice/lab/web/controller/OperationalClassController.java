6513e569a0c47646a7a480a2a899a963d152a654
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2023-02-17 09:00:40 +0700

    update: api in url controller

--- src/main/java/com/microservice/lab/web/controller/OperationalClassController.java
+++ src/main/java/com/microservice/lab/web/controller/OperationalClassController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/operational-class")
+@RequestMapping("/api/operational-class")
 public class OperationalClassController {
     private OperationalClassService operationalClassService;
     @Autowired
