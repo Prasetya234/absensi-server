6aee5a1da3dc02a12bdba725bd4367eab973ab50
Prasetya234 <putraprasetyadhany@gmail.com>
2023-03-06 02:51:59 +0700

    test bot run on server

--- src/main/java/com/microservice/lab/telegrambot/RecoveryMessageBot.java
+++ src/main/java/com/microservice/lab/telegrambot/RecoveryMessageBot.java
@@ -9,13 +9,16 @@ import java.util.Date;
 @Component
 public class RecoveryMessageBot {
         public void sendMessage(Date date, String ipAddress, String token, String message) {
+            StringBuilder str = new StringBuilder();
+            str.append("https://api.telegram.org/bot5458201499:AAFxB77rGvyATgnMnIOxeAU7w7SnlBqyLSg/sendMessage?chat_id=@absensierrorstaging&text=");
             String space = "\n";
             String dateMsg = "Timestamp: " + date.toString();
             String ipAddressMsg = "IpAddress: " + ipAddress;
             String tokenMsg = "Token: " + token;
             String messageMsg = "Message: " + message;
             final String messageFinal =  dateMsg + space + ipAddressMsg + space + tokenMsg + space + messageMsg;
-            final String uri = "https://api.telegram.org/bot5458201499:AAFxB77rGvyATgnMnIOxeAU7w7SnlBqyLSg/sendMessage?chat_id=@absensierrorstaging&text=" + messageFinal;
+            str.append(messageFinal);
+            final String uri = str.toString();
             RestTemplate restTemplate = new RestTemplate();
            restTemplate.getForObject(uri, String.class);
         }
