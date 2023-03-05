e3e383d1972b605b6021b321d1fc22d3f47511da
Prasetya234 <putraprasetyadhany@gmail.com>
2023-03-06 02:59:10 +0700

    test bot run on server 2

--- src/main/java/com/microservice/lab/telegrambot/RecoveryMessageBot.java
+++ src/main/java/com/microservice/lab/telegrambot/RecoveryMessageBot.java
@@ -1,25 +1,31 @@
 package com.microservice.lab.telegrambot;
 
 
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @Component
 public class RecoveryMessageBot {
-        public void sendMessage(Date date, String ipAddress, String token, String message) {
-            StringBuilder str = new StringBuilder();
-            str.append("https://api.telegram.org/bot5458201499:AAFxB77rGvyATgnMnIOxeAU7w7SnlBqyLSg/sendMessage?chat_id=@absensierrorstaging&text=");
-            String space = "\n";
-            String dateMsg = "Timestamp: " + date.toString();
-            String ipAddressMsg = "IpAddress: " + ipAddress;
-            String tokenMsg = "Token: " + token;
-            String messageMsg = "Message: " + message;
-            final String messageFinal =  dateMsg + space + ipAddressMsg + space + tokenMsg + space + messageMsg;
-            str.append(messageFinal);
-            final String uri = str.toString();
-            RestTemplate restTemplate = new RestTemplate();
-           restTemplate.getForObject(uri, String.class);
-        }
+    public void sendMessage(Date date, String ipAddress, String token, String message) {
+        StringBuilder str = new StringBuilder();
+        str.append("https://api.telegram.org/bot5458201499:AAFxB77rGvyATgnMnIOxeAU7w7SnlBqyLSg/sendMessage?chat_id=@absensierrorstaging&text=");
+        String space = "\n";
+        String dateMsg = "Timestamp: " + date.toString();
+        String ipAddressMsg = "IpAddress: " + ipAddress;
+        String tokenMsg = "Token: " + token;
+        String messageMsg = "Message: " + message;
+        final String messageFinal = dateMsg + space + ipAddressMsg + space + tokenMsg + space + messageMsg;
+        str.append(messageFinal);
+        final String uri = str.toString();
+        RestTemplate restTemplate = new RestTemplate();
+        final MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        mappingJackson2HttpMessageConverter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_OCTET_STREAM));
+        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter);
+        restTemplate.getForObject(uri, String.class);
+    }
 }
